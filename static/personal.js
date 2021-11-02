d19fe9ddedd7573a6e17cacee315f6e5c87e2adc
Simon-157 <boatengsimonjnr157@gmail.com>
2021-11-02 09:58:16 +0000

    Added responsiveness

---
+++ static/personal.js
@@ -0,0 +1,27 @@
+window.scroll({
+  top: 1000,
+  behavior: 'smooth'  // 👈 
+});
+
+
+
+
+const observer = new IntersectionObserver(entries =>{
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      document.querySelectorAll(".project")[0].classList.add("fadeIn")
+      document.querySelectorAll(".project")[1].classList.add("fadeIn")
+      document.querySelectorAll(".project")[2].classList.add("fadeIn")
+      document.querySelectorAll(".project")[3].classList.add("fadeIn")
+    }
+  })
+})
+
+observer.observe(document.querySelector(".container"));
+
+
+
+
+
+
+
